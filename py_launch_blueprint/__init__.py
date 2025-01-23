7da08871bca891dacf479c0a8744f0b28239ff00
Adane Moges <adman19940805@gmail.com>
2025-01-23 22:52:52 +0300

    Update .gitignore

--- py_launch_blueprint/__init__.py
+++ py_launch_blueprint/__init__.py
@@ -2,6 +2,4 @@
 
 from importlib import metadata
 
-__version__ = metadata.version("python-utils")
-
-from .projects import main  # Re-export main function
+__version__ = metadata.version("py-utils")
