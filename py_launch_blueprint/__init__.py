7e78f3e70a05207a0d0f932eb02d80dd7272b6da
Adane Moges <adman19940805@gmail.com>
2025-01-24 08:59:44 +0300

    fix: Refactor lint configuration and re-export main function

--- py_launch_blueprint/__init__.py
+++ py_launch_blueprint/__init__.py
@@ -4,4 +4,4 @@ from importlib import metadata
 
 __version__ = metadata.version("py_launch_blueprint")
 
-from .projects import main  # Re-export main function
+from .projects import main as main  # Re-export main function
