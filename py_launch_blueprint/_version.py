215d90ea111af7fe8483712cb857c410147f8657
Tatiana H. <tatihe3@gmail.com>
2025-05-07 16:36:20 -0600

    chore: update Ruff config to ignore version.py

--- py_launch_blueprint/_version.py
+++ py_launch_blueprint/_version.py
@@ -5,7 +5,10 @@ __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    VERSION_TUPLE = tuple[int | str, ...]
+    from typing import Tuple
+    from typing import Union
+
+    VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
     VERSION_TUPLE = object
 
@@ -14,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev247"
-__version_tuple__ = version_tuple = (0, 1, "dev247")
+__version__ = version = "0.1.dev283"
+__version_tuple__ = version_tuple = (0, 1, "dev283")
