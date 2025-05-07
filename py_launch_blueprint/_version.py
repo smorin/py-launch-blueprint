75318ea53c6991bce78fa839708e62fe7864fae2
Tatiana H. <tatihe3@gmail.com>
2025-05-07 17:48:57 -0600

    Changing deprecated fixes

--- py_launch_blueprint/_version.py
+++ py_launch_blueprint/_version.py
@@ -5,7 +5,9 @@ __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    VERSION_TUPLE = tuple[int | str, ...]
+    from typing import Union
+
+    VERSION_TUPLE = tuple[Union[int, str], ...]
 else:
     VERSION_TUPLE = object
 
@@ -14,5 +16,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev283"
-__version_tuple__ = version_tuple = (0, 1, "dev283")
+__version__ = version = "0.1.dev275"
+__version_tuple__ = version_tuple = (0, 1, "dev275")
